ea6278b7ae9b82e9d580f30459be4a8de723ee78
KanchanCS <kanchan111yadav@gmail.com>
2024-02-03 14:20:15 +0530

    add

--- book/forms.py
+++ book/forms.py
@@ -4,7 +4,14 @@ from .models import Contact
 
 
 class  ContactForm(ModelForm):
+    
+
     class Meta:
         model = Contact
         fields = '__all__'
         exclude = ['user']
+        
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs["class"] = "form-control"
--- book/urls.py
+++ book/urls.py
@@ -7,7 +7,7 @@ app_name = "book"
 urlpatterns = [
     path("", views.home, name="home"),
     path("create/", views.create, name="create"),
-     path("edit/<int:id>/", views.edit_contact, name="edit_contact"),
-     path("delete/<int:id>/", views.delete_contact, name="delete_contact"),
-      path("details/<int:id>/", views.contact_details, name="contact_details"),
+    path("edit/<int:id>/", views.edit_contact, name="edit_contact"),
+    path("delete/<int:id>/", views.delete_contact, name="delete_contact"),
+    path("details/<int:id>/", views.contact_details, name="contact_details"),
 ]
